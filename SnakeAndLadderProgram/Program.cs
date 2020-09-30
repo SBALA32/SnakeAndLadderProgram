d313cd8bb55895d0dea0e8a08e1d9919eff93f5b
Sudipta <sudiptabala32@gmail.com>
2020-10-01 01:12:36 +0530

    [Sudipta] Add : Rolling a die for the first time

--- SnakeAndLadderProgram/Program.cs
+++ SnakeAndLadderProgram/Program.cs
@@ -3,9 +3,17 @@ namespace SnakeAndLadderSimulator
 {
     class Program
     {
+        //Returns Output Of Die Roll
+        public static int rollTheDie()
+        {
+            Random random = new Random();
+            int numberOnDie = random.Next(1, 7);
+            return numberOnDie;
+        }
         static void Main(string[] args)
         {
             int currentPosition = 0;
+            int numberOnDie = rollTheDie();
         }
     }
 }
