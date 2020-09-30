2251b23314fd71af0460f272f30db34044a29dc3
Sudipta <sudiptabala32@gmail.com>
2020-10-01 01:08:34 +0530

    [Sudipta] Add : Starting Position set to zero

---
+++ SnakeAndLadderProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SnakeAndLadderSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentPosition = 0;
+        }
+    }
+}
